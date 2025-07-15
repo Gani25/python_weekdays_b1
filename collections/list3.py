0f0cd4942146aaf3c07388a1cc4bb749b2426c65
Gani25 <memonabdulgani3d@gmail.com>
2025-07-15 15:52:44 +0530

    List More Examples

---
+++ collections/list3.py
@@ -0,0 +1,5 @@
+my_list = [5,1,7,9,3]
+
+# Looping over list
+for i in my_list:
+    print(i)
---
+++ collections/list4.py
@@ -0,0 +1,16 @@
+# sum of element present in list
+numbers = [2,10,25,30,80,65,80]
+
+# sum = 0
+# for element in numbers:
+    # sum = sum + element
+
+
+# print(f"Sum of elements is {sum}")
+print(f"Sum of elements with function is {sum(numbers)}")
+print(f"Maximum elements is {max(numbers)}")
+
+# Find sum of odd and even element present in list
+# [5,1,7,2,4,6,9,3]
+# odd = 5+1+7+9+3
+# even = 2+4+6
