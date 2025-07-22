c066aae0db7f2f18724867f74a466b0698066a36
Gani25 <memonabdulgani3d@gmail.com>
2025-07-22 16:15:25 +0530

    class and objects

--- oops/cricket_player_main.py
+++ oops/cricket_player_main.py
@@ -1,23 +1,17 @@
 # import cricket_player_module as cpm
-from cricket_player_module import cricket_player,calculate_avg
+from cricket_player_module import cricket_player
 # from cricket_player_module import *
 
 # virat = cricket_player_module.cricket_player()
 
 # virat = cpm.cricket_player
-virat = cricket_player(birth_year=1987, first_name="Virat", last_name="Kohli",team="India")
+player1 = cricket_player(birth_year=1987, first_name="Virat", last_name="Kohli",team="India")
+player1.add_scores(50,80,60)
+player1.add_scores(30)
+print(player1)
 
-print("Player Info")
-print("Name =",virat.first_name ,virat.last_name)
-print("Team =",virat.team)
-print("Birth Year =",virat.birth_year)
-print("Scores =",virat.scores)
+player2 = cricket_player("David","Warner",1985,"Australia")
+player2.add_scores(80,0,50,30,25,100)
+print(player2)
 
-
-david = cricket_player(birth_year=1985, first_name="David", last_name="Warner",team="Australia")
-print("\nPlayer Info")
-print("Name =",david.first_name ,david.last_name)
-print("Team =",david.team)
-print("Birth Year =",david.birth_year)
-print("Scores =",david.scores)
 
--- oops/cricket_player_module.py
+++ oops/cricket_player_module.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class cricket_player:
     # FIELDS
     
@@ -9,5 +10,21 @@ class cricket_player:
         self.team = team
         self.scores = []
 
-def calculate_avg():
-    return 0
+    # OVERRIDE
+    def __str__(self):
+        return f"-----------------------------------------------\n~~~Player Info~~~\n-----------------------------------------------\nName = {self.first_name} {self.last_name}\nTeam = {self.team}\nBirth Year = {self.birth_year}\nAge = {self.player_age()}\nScores = {self.scores}\nAverage Scores = {self.calculate_average()}"
+
+    def player_age(self):
+        return datetime.now().year - self.birth_year
+    
+    def add_scores(self,*runs):
+        self.scores.extend(runs)
+    
+    def calculate_average(self):
+        return sum(self.scores) / len(self.scores)
+
+    
+
+# def player_age(birth_year:int) -> int:
+
+# CLASS for Football Player -> first name, last name, birth year, goal
